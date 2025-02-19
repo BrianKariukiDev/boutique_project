4befc61a359cc17f6ce9b295a8b1bc2e36945447
Brian Kariuki <kariukibrian087@gmail.com>
2025-02-19 19:52:07 +0300

    delivery migration

---
+++ database/migrations/2025_02_19_164703_create_deliveries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->foreignId('address_id')->constrained('addresses')->cascadeOnDelete();
+            $table->boolean('is_pickupPoint')->default(false);
+            $table->foreignId('nearest_pickup_point_id')->nullable()->constrained('pickup_points')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
