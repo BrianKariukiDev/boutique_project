e8230de57440e5192e656a2d10fd02ec491962c8
Brian Kariuki <kariukibrian087@gmail.com>
2025-02-25 07:34:29 +0300

    Order Relation Manager

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\OrderRelationManager;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Components\DateTimePicker;
@@ -104,7 +105,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+           OrderRelationManager::class 
         ];
     }
 
---
+++ app/Filament/Resources/UserResource/RelationManagers/OrderRelationManager.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Resources\OrderResource;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderRelationManager extends RelationManager
+{
+    protected static string $relationship = 'orders';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('Order ID')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('grand_total')
+                    ->label('Grand Total')
+                    ->sortable()
+                    ->searchable()
+                    ->money('Kshs'),
+
+                TextColumn::make('status')
+                    ->badge()
+                    ->sortable()
+                    ->color(fn($state)=>match($state){
+                        'new'=>'info',
+                        'processing'=>'warning',
+                        'shipped'=>'success',
+                        'delivered'=>'success',
+                        'canceled'=>'danger',
+                    })
+
+                    ->icon(fn($state)=>match($state){
+                        'new'=>'heroicon-m-sparkles',
+                        'processing'=>'heroicon-m-arrow-path',
+                        'shipped'=>'heroicon-m-truck',
+                        'delivered'=>'heroicon-m-check-badge',
+                        'canceled'=>'heroicon-m-x-circle',
+                    }),
+
+                TextColumn::make('payment_method')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('payment_status')
+                    ->searchable()
+                    ->sortable()
+                    ->badge(),
+
+                TextColumn::make('created_at')
+                    ->searchable()
+                    ->sortable()
+                    ->dateTime()
+                    ->label('Order Date'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make()
+                    ->url(fn($record)=>OrderResource::getUrl('view',['view','record'=>$record]))
+                    ->color('info')
+                    ->icon('heroicon-o-eye'),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
