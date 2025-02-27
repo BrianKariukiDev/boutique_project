a8c41d5961f55296bed79893df4fc668acab9c58
Brian Kariuki <kariukibrian087@gmail.com>
2025-02-27 14:06:34 +0300

    making category,brands and latest dynamic

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
 
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands=Brand::where('is_active','1')->get();
+        $categories=Category::where('is_active','1')->get();
+        $latestProducts=Product::latest()->limit(6)->get();
+        return view('livewire.home-page',[
+            'brands'=>$brands,
+            'categories'=>$categories,
+            'latestProducts'=>$latestProducts,
+        ]);
     }
 }
