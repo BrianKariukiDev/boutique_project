d3c93566b79ca443648e711ff9f33f6a77df5179
Brian Kariuki <kariukibrian087@gmail.com>
2025-03-06 14:35:25 +0300

    Working on cart

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
 use Livewire\Component;
 
 class CartPage extends Component
 {
+    public $cartItems=[];
+    public $grandTotal=0;
+
+    public function mount()
+    {
+        $this->cartItems= CartManagement::getCartItemsFromCookie();
+
+        $this->grandTotal=CartManagement::calculateGrandTotal();
+
+    }
     public function render()
     {
         return view('livewire.cart-page');
